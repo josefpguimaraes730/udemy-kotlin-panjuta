0bfb05c8fea63bc524429ec571ab4b3529bc5ec9
josefpguimaraes730 <josepereira730@gmail.com>
2024-06-06 15:46:51 -0300

    Surface

--- app/src/main/java/br/jguimaraes/myfirstapp/MainActivity.kt
+++ app/src/main/java/br/jguimaraes/myfirstapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.jguimaraes.myfirstapp
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,10 +22,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyfirstappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                //    Greeting(
+                //            name = "Android",
+                //            modifier = Modifier.padding(innerPadding)
+                //    )
+                //}
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
+                        name = "Android",
                     )
                 }
             }
