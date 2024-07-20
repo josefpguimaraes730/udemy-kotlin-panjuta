8d31fe03f7a89613820ae4bb2fa97d31f4af1c65
josefpguimaraes730 <josepereira730@gmail.com>
2024-07-20 02:04:22 -0300

    create app screen components

--- app/src/main/java/br/jguimaraes/unitconverter/MainActivity.kt
+++ app/src/main/java/br/jguimaraes/unitconverter/MainActivity.kt
@@ -1,18 +1,39 @@
 package br.jguimaraes.unitconverter
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.pm.ShortcutInfoCompat
 import br.jguimaraes.unitconverter.ui.theme.UnitConverterTheme
 
@@ -32,9 +53,112 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Android")
+                    UnitConverter()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Here all the UI elements will be stacked below ea ch other
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {
+            // Here goes what should happen, when the 'value' changes
+        }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            // Here all the UI elements will be stacked next to each other
+            val context = LocalContext.current
+            // Button(onClick = { Toast.makeText(context,
+            //     "Thanks for clicking!",
+            //     Toast.LENGTH_LONG).show()
+            // }
+            // ) {
+            //     Text(text = "Click Me!")
+            // }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false,
+                    onDismissRequest = {  }
+                ) {
+                    DropdownMenuItem(text = { Text("Centimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Meters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Feet") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Milimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false,
+                    onDismissRequest = {  }
+                ) {
+                    DropdownMenuItem(text = { Text("Centimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Meters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Feet") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(text = { Text("Milimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                }
+            }
+        }
+
+        //Spacer(modifier = Modifier.height(16.dp))
+        Text("Result:",
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UnitConverterPreview() {
+    UnitConverter()
+}
+
+@Composable
+fun GridSample() {
+    Column {
+        // Here all the UI elements will be stacked below ea ch other
+        Greeting(name = "Android")
+        Row {
+            // Here all the UI elements will be stacked next to each other
+            Greeting(name = "Kotlin")
+            Greeting(name = "Joe")
         }
     }
 }
