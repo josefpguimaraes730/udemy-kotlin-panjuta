d39c55d0727ef9d7971d96875cb0df2f4fb97c4f
josefpguimaraes730 <josepereira730@gmail.com>
2024-07-20 00:17:37 -0300

    run app

--- app/src/main/java/br/jguimaraes/unitconverter/MainActivity.kt
+++ app/src/main/java/br/jguimaraes/unitconverter/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.pm.ShortcutInfoCompat
 import br.jguimaraes.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +22,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                //     Greeting(
+                //         name = "Android",
+                //         modifier = Modifier.padding(innerPadding)
+                //     )
+                // }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Android")
                 }
             }
         }
